26cad00d5d6c7fa07dc4b7d5c61ce4aac44ea0e8
superrubydeveloper <grandpa0064@gmail.com>
2020-07-28 03:07:30 -0500

    updated routes, registrations, and shot pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,15 @@
 Rails.application.routes.draw do
-  resources :shots
+  resources :shots do 
+  	resources :comments 
+  	member do 
+  		put 'like', to: "shots#like"
+  		put 'unlike', to: "shots#unlike"
+  	end
+  end
+ 
+  devise_for :users, controllers: { registrations: 'registrations' }
+  
+  root 'shots#index'
 
-  devise_for :users, controller: { registrations: 'registrations'}
-
-  root 'home#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
