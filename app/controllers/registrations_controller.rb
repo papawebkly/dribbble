ff30ecc1a82a376ca824cc292b5178b0ff851e4f
superrubydeveloper <grandpa0064@gmail.com>
2020-07-29 01:22:26 -0500

    generated shot uploader

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -11,4 +11,4 @@ class RegistrationsController < Devise::RegistrationsController
        params.require(:user).permit(:name, :email, :password, :password_confirmation, :current_password)
      end
    
-   end
+end
